5151c3a7af96df22eb30e7649e5ea08c843e671b
natalabr <natala.brys@gmail.com>
2025-05-23 09:33:27 +0200

    Recover /src dir to main.

---
+++ projekt/projekt.client/src/pages/NewTrail.jsx
@@ -0,0 +1,14 @@
+import '../styles/New.css';
+import Header from '../components/Header';
+import New from '../components/New';
+import Footer from '../components/Footer';
+
+export default function App() {
+    return (
+        <div>
+            <Header/>
+            <New/>
+            <Footer/>
+        </div>
+    );
+}
